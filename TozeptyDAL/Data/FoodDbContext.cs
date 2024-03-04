3583b11c4e7aa7ae0406c4fce4f5e88914a22fa5
dheeraj kumar <dheeraj kumar@DHEERAJCHOWDARY>
2024-03-05 01:50:38 +0530

    created order model

--- TozeptyDAL/Data/FoodDbContext.cs
+++ TozeptyDAL/Data/FoodDbContext.cs
@@ -21,7 +21,7 @@ namespace TozeptyDAL.Data
 
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Customer> Customers { get; set; }
-
+        public DbSet<Order> Orders { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             // Configure the relationship between Customer and Order
---
+++ TozeptyDAL/Migrations/202403042017091_ordertable.cs
@@ -0,0 +1,32 @@
+﻿namespace TozeptyDAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ordertable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderID = c.Int(nullable: false, identity: true),
+                        CustomerID = c.Int(nullable: false),
+                        OrderDate = c.DateTime(nullable: false),
+                        ProductID = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        OrderStatus = c.String(),
+                        PaymentStatus = c.String(),
+                    })
+                .PrimaryKey(t => t.OrderID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Orders");
+        }
+    }
+}
