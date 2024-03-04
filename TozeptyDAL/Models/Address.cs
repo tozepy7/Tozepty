500693418b8276389af5ea6bdb97aeb90da6fca1
Lakshay <thelakshaymehta@gmail.com>
2024-03-05 03:01:51 +0530

    Created Repository for Address

---
+++ TozeptyDAL/Models/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TozeptyDAL.Models
+{
+    public class Address
+    {
+        //Address -> AddressID, Street, City, State, Country , CustomerID
+
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+    }
+}
---
+++ TozeptyDAL/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TozeptyDAL.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        //[Index(IsUnique = true)]
+        [MaxLength(255)]
+        public string CategoryName { get; set; }
+
+        // Navigation property
+        public ICollection<Product> Products { get; set; }
+    }
+}
