8bfc67d290804ea463439db53b4d219691ffa45f
KusumaNaiduB <kusumanaidu8@gmail.com>
2024-03-05 03:57:56 +0530

    Created Interface and repostority for cart

---
+++ TozeptyDAL/Interfaces/ICartRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TozeptyDAL.Models;
+
+namespace TozeptyDAL.Interfaces
+{
+    public interface ICartRepository
+    {
+        // Create
+        Cart CreateCartItem(Cart cartItem);
+        Cart GetCartItemByCartIdAndCustomerId(int cartId, int customerId);
+        bool cartStatus();
+
+        // Read
+        Cart GetCartItemById(int cartItemId);
+        IEnumerable<Cart> GetAllCartItems();
+
+        // Update
+        Cart UpdateCartItem(Cart cartItem);
+
+        // Delete
+        Cart DeleteCartItem(int cartItemId);
+        IEnumerable<Cart> GetCartItemById(int[] cartIds);
+        int cartSaveChanges();
+        List<Cart> GetCartItemsByCustomerId(int? customerId);
+
+        void DeleteCartItems(List<Cart> cartItems);
+        Cart RemoveCartItem(Cart cartItem);
+    }
+}
