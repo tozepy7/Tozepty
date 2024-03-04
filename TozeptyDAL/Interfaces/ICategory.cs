02f97307f83de89af539845300575a552b98cd7b
Manjula36 <manjulavdt@gmail.com>
2024-03-05 04:22:39 +0530

    AddedRepository and interface for category

---
+++ TozeptyDAL/Interfaces/ICategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TozeptyDAL.Models;
+
+namespace TozeptyDAL.Interfaces
+{
+    public interface ICategory
+    {
+        int GetLastCategoryId();
+        int SaveCategory(Category category);
+        IEnumerable<Category> GetAllCategories();
+        bool CategoryExists(string categoryName);
+        bool CategoryExists(string categoryName, int id);
+        Category GetCategoryById(int id);
+        Category UpdateCategory(Category existingCategory);
+        void DeleteCategory(Category categoryToDelete);
+    }
+}
