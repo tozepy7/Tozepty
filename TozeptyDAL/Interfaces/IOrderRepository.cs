f0ed78394cf347bf37587d0ce81e43fd3f76e965
dheeraj kumar <dheeraj kumar@DHEERAJCHOWDARY>
2024-03-05 06:42:54 +0530

    orderrepos

---
+++ TozeptyDAL/Interfaces/IOrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TozeptyDAL.Models;
+
+namespace TozeptyDAL.Interfaces
+{
+    public interface IOrderRepository
+    {
+        // Method to create an order
+        void CreateOrder(Order order);
+
+        // Another method to create an order with a different signature
+        Order CreateOrder2(Order order);
+
+        // Method to get an order by its ID
+        Order GetOrderById(int orderId);
+
+        // Method to get all orders
+        IEnumerable<Order> GetAllOrders();
+
+        // Method to update an order
+        void UpdateOrder(Order order);
+
+        // Method to delete an order
+        void DeleteOrder(Order order);
+
+        // Method to get orders by customer ID
+        IEnumerable<Order> GetOrdersByCustomerId(int userId);
+
+        // Method to get incomplete orders
+        IEnumerable<Order> GetIncompleteOrders();
+    }
+}
---
+++ TozeptyDAL/Repository/OrderRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using TozeptyDAL.Data;
+using TozeptyDAL.Interfaces;
+using TozeptyDAL.Models;
+
+namespace FoodDeliveryDAL.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly FoodDbContext _context;
+
+        public OrderRepository(FoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+        }
+
+        public Order CreateOrder2(Order order)
+        {
+            var addedOrder = _context.Orders.Add(order);
+            _context.SaveChanges();
+            return addedOrder;
+        }
+
+        public Order GetOrderById(int orderId)
+        {
+            return _context.Orders.Find(orderId);
+        }
+
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return _context.Orders.ToList();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            _context.Entry(order).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteOrder(Order order)
+        {
+            _context.Orders.Remove(order);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Order> GetOrdersByCustomerId(int userId)
+        {
+            IEnumerable<Order> orders = _context.Orders
+                .Include(o => o.OrderDetails)
+                .Where(o => o.CustomerId == userId)
+                .ToList();
+
+            return orders;
+        }
+
+        public IEnumerable<Order> GetIncompleteOrders()
+        {
+            IEnumerable<Order> orders = _context.Orders
+                .Include(o => o.OrderDetails)
+                .Where(o => o.OrderDetails.Any(od => od.OrderStatus < 4))
+                .ToList();
+
+            foreach (var order in orders)
+            {
+                order.OrderDetails = order.OrderDetails
+                    .Where(od => od.OrderStatus < 4)
+                    .GroupBy(od => od.OrderId)
+                    .Select(g => g.First())
+                    .ToList();
+            }
+
+            return orders;
+        }
+    }
+}
