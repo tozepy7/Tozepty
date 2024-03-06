131c2d7ef4d14fc98fdf1e99b1aa23dba4506c2e
Lakshay <thelakshaymehta@gmail.com>
2024-03-06 07:05:43 +0530

    Added Account Controller

---
+++ Tozepty/Models/UserViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tozepty.Models
+{
+    public class UserViewModel
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "First name is required.")]
+        [StringLength(50, ErrorMessage = "First name cannot exceed 50 characters.")]
+        [RegularExpression(@"^[A-Za-z]{3,15}$", ErrorMessage = "First name can only contain alphabets and must be between 3 to 15 characters.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [StringLength(50, ErrorMessage = "Last name cannot exceed 50 characters.")]
+        [RegularExpression(@"^[A-Za-z]{3,15}$", ErrorMessage = "Last name can only contain alphabets and must be between 3 to 15 characters.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email address is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [StringLength(255, ErrorMessage = "Email address cannot exceed 255 characters.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [RegularExpression(@"^[6-9][0-9]{9}$", ErrorMessage = "Invalid phone number format.")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(15, MinimumLength = 4, ErrorMessage = "Username must be between 4 to 15 characters.")]
+        [RegularExpression(@"^[A-Za-z0-9_-]+$", ErrorMessage = "Username can only contain letters, numbers, underscores, and hyphens.")]
+        public string UserName { get; set; }
+
+    }
+}
