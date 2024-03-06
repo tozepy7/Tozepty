bbda5bfb95b587ab5ff6baa7ec1723f94527e894
raj <raj@gmail.com>
2024-03-06 06:36:46 +0530

    errorcontrollercreated

---
+++ Tozepty/Controllers/ErrorpageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tozepty.Controllers
+{
+    public class ErrorpageController : Controller
+    {
+        // GET: Errorpage
+        public ActionResult UnauthorisedAccess()
+        {
+            return View();
+        }
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
--- Tozepty/Global.asax.cs
+++ Tozepty/Global.asax.cs
@@ -17,5 +17,11 @@ namespace Tozepty
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exc = Server.GetLastError();
+            Server.ClearError();
+            Response.Redirect("/ErrorPage/ErrorMessage");
+        }
     }
 }
